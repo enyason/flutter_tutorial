eac601bc164cfba1909fd20fb048bea96caa5113
enyason <enyasonjnr@yahoo.com>
2019-01-28 00:38:10 +0100

    customized list items

--- lib/List and Dialogs.dart
+++ lib/List and Dialogs.dart
@@ -8,21 +8,68 @@ class ListDialog extends StatefulWidget {
 class _ListDialogState extends State<ListDialog> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Center(
-          child: Text("Emmanuel"),
-        ),
-        Center(
-          child: Text("Ideba "),
-        ),
-        Center(
-          child: Text("Enya"),
-        ),
-        Center(
-          child: Text("enyasonjnr@gmail.com"),
-        ),
-      ],
+    // using ListView manually
+//    return ListView(
+//      children: <Widget>[
+//        Center(
+//          child: Text("Emmanuel"),
+//        ),
+//        Center(
+//          child: Text("Ideba "),
+//        ),
+//        Center(
+//          child: Text("Enya"),
+//        ),
+//        Center(
+//          child: Text("enyasonjnr@gmail.com"),
+//        ),
+//      ],
+//    );
+
+    // creating an infinite loop
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text("enyason"),
+          subtitle: Row(
+            children: <Widget>[
+              CircleAvatar(radius: 3.0,backgroundColor: Colors.green,),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: Text("active"),
+              )
+            ],
+          ),
+          leading: Icon(Icons.person),
+          trailing: Icon(Icons.delete),
+          onTap: () {
+
+            showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext ctx) {
+
+                  return AlertDialog(
+                    actions: <Widget>[
+                      new FlatButton(onPressed: (){
+                        Navigator.of(context).pop(true);
+                      }, child: Text("Yes")),
+                      new FlatButton(onPressed: (){
+                        Navigator.of(context).pop(false);
+
+                      }, child: Text("No"))
+                    ],
+                    content: Text("Continue to delete user"),
+                  );
+
+                }).then((value)=> print("the value is $value"));
+
+
+          },
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(title: Text("List Demo"),leading: Icon(Icons.featured_play_list),),
         body: Container(
           child: ListDialog(),
         ),
